3661fbe5fba8db020f168d421393ce110cbab3be
hariom joshi <159013690+Hariom5266@users.noreply.github.com>
2024-07-13 14:02:36 +0800

    Add files via upload
    On the first day of the 30-day JavaScript challenge, I explored the fundamental data types in JavaScript. Understanding these data types is crucial for building a solid foundation in programming and effectively manipulating data.

---
+++ DAY_01/01.js
@@ -0,0 +1,45 @@
+// 🙏🏻 , This is First Day of 30 Days Challenge of JS
+
+// # ======================= DATA TYPES  ============================= #
+
+// ACTIVITY 1
+
+var number = 13;//it is accesible whole program
+console.log(number);
+
+let str = "HARIOM"; // it has only block scope
+console.log(str);
+
+// ACTIVITY 2
+
+const surname = "JOSHI";
+console.log(surname);
+
+// ACTIVITY 3
+
+let num = 13;
+let strVar = "Hariom Joshi";
+let bool = true;
+let obj = {
+    name : "Hariom Joshi",
+    role : "Frontend Web Devloper"
+}
+let arr = ['Hariom','chetanbhai','joshi'];
+
+console.log(typeof num);
+console.log(typeof strVar);
+console.log(typeof bool);
+console.log(typeof obj);
+console.log(typeof arr);
+
+// ACTIVITY 4
+
+let varvalue = 'hi i am reasign var';
+console.log(varvalue);
+varvalue = 'i reassigned';
+console.log(varvalue);
+
+// ACTIVITY 5 // THIS THROUGH ERROR LIKR THIS : TypeError: Assignment to constant variable.
+
+surname = "Joshi";
+console.log(surname);
